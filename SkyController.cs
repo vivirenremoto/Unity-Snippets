7a487c39b0c8631c51557a6328403e88d691faca
vivirenremoto <gafeman@gmail.com>
2022-10-04 16:08:59 +0200

    Create SkyController.cs

---
+++ SkyController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyController : MonoBehaviour
+{
+
+    public float speed;
+
+    void Start()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", 0);
+    }
+
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * speed);
+    }
+
+}
